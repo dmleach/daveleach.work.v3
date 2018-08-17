ce385eb50d2e813f1e600325f9414d28b54c1145
Dave Leach <dave@daveleach.work>
2018-08-16 23:10:20 -0700

    Added contact information

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -25,10 +25,22 @@
             </nav>
 
             <div id="main">
-                @section('content')
-                    Base page content
-                @show
+                <article id="home" class="panel">
+                    @section('content')
+                        Base page content
+                    @show
+                </article>
             </div>
         </div>
+        <div id="footer">
+            <ul class="copyright">
+                <li>&copy; 2018 Dave Leach</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </div>
+        <!-- <script src="js/astral/jquery.min.js"></script>
+        <script src="js/astral/browser.min.js"></script>
+        <script src="js/astral/breakpoints.min.js"></script>
+        <script src="js/astral/util.js"></script>
+        <script src="js/astral/main.js"></script> -->
     </body>
 </html>
